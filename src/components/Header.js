b8e96f7a6183280668d7b661e45ce2e00a03fa1f
Gururagavendra V <guruvelu85@gmail.com>
2023-10-01 11:16:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [state, setState] = useState(true);
+  const handleclick = () => {
+    setState(!state);
+  };
   return (
     <>
       <nav className="nav">
         <a href="/">Netflix</a>
         <div>
-          <ul className="nav-ul">
+          <ul className={state ? "nav-ul" : "nav-ul active"}>
             <li>Home</li>
             <li>Movies</li>
             <li>Ratings</li>
             <li>Help</li>
           </ul>
+        </div>
+        <div id="mobile">
+          <i
+            onClick={handleclick}
+            className={state ? "fas fa-bars" : "fas fa-times"}
+          ></i>
         </div>
       </nav>
     </>
